ec441d7a7e8c0797d5503730c78ec7ae8d1c8668
karrmao <karrmao@gmail.com>
2022-07-15 21:36:19 +0300

    add

---
+++ 21_DOM/Task1H/index.js
@@ -0,0 +1,8 @@
+const getTitleElement = () => {
+  const titleElem = document.querySelector('.title');
+  console.dir(titleElem);
+  return titleElem;
+};
+
+//console.log('hello');
+//console.log(getTitleElement);
