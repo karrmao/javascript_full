9563db145779b12108ed97c1baa500ff6da5c5e7
karrmao <karrmao@gmail.com>
2022-07-15 17:33:15 +0300

    add

--- 21_DOM/main.js
+++ 21_DOM/main.js
@@ -1,19 +1,50 @@
-// /*********************19_prototypes***********************************/
-// /*1*/
+// /*********************21_DOM***********************************/
+//Task1 from lesson
+'use strict';
 
-// console.log('/***********************************************/');
-// /***************************************************************/
-// /*2*/
+/*export*/ const tasks = [
+  { text: 'Buy milk', done: false },
+  { text: 'Pick up Tom from airport', done: false },
+  { text: 'Visit party', done: false },
+  { text: 'Visit doctor', done: true },
+  { text: 'Buy meat', done: true },
+];
 
-// console.log('/***********************************************/');
-// /***************************************************************/
-// /*3*/
+/**
+ * @param {object[]} tasksList
+ * @return {undefined}
+ */
 
-// console.log('/***********************************************/');
-// /***************************************************************/
+const renderTasks = tasksList => {
+  // put your code here
+  const listElem = document.querySelector('.list'); /*1*/
 
-// /*1H*/
+  const listItemsElems = tasksList
+    .sort((a, b) => a.done - b.done)
+    .map(({ text, done }) => {
+      const listItemElem = document.createElement('li'); /*2*/
+      listItemElem.classList.add('list__item');
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
+      const checkboxElem = document.createElement('input');
+      checkboxElem.setAttribute('type', 'checkbox');
+      checkboxElem.checked = done;
+      checkboxElem.classList.add('list__item-checkbox');
+      listItemElem.append(checkboxElem, text);
 
-// console.log('/***********************************************/');
-// /***************************************************************/
-/*2H*/
+      return listItemElem;
+    });
+
+  listElem.append(...listItemsElems);
+};
+
+renderTasks(tasks);
+
+/*Формуємо html елементи li через .createElement(добавляємо його у змінну)*/
+/*Отримуємо доступ до .list через .querySelector(добавляємо його у змінну)*/
+
+//console.log('/***********************************************/');
+
+/*1*/
+/*2*/
