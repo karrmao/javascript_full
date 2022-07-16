9c516b4ebcc047e9ebffb6bef580c7ed70938755
karrmao <karrmao@gmail.com>
2022-07-16 19:16:30 +0300

    add

--- 21_DOM/Task5H/index.js
+++ 21_DOM/Task5H/index.js
@@ -1,13 +1,5 @@
-// /*export*/ const setTitle = text => {
-//   document.querySelector('.title').textContent = text;
-//   //   console.dir(addText);
-//   //return addText;
-// };
-
-// export const setButton = buttonText => {
-//   document.querySelector('body').innerHTML = 'buttonText';
-// };
-
-const srtButton = buttonText => {
+export const srtButton = buttonText => {
   document.querySelector('body').innerHTML = `<button>${buttonText}</button>`;
 };
+
+// console.log(srtButton('Просто кнопка'));
