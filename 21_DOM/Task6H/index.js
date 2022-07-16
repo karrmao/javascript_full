5ddc951cff81307b7d5950ad4f96f5697d8f7958
karrmao <karrmao@gmail.com>
2022-07-16 20:04:54 +0300

    change

--- 21_DOM/Task6H/index.js
+++ 21_DOM/Task6H/index.js
@@ -1,12 +1,3 @@
-// /*export*/ const setButton = buttonText => {
-//   const res = (document.querySelector('body').innerHTML = `<button>${buttonText}</button>`);
-//   console.dir(res);
-// };
-
-// setButton('Просто кнопка');
-
-//console.log('bb');
-
 export const clearList = () => {
   document.querySelector('.categories').innerHTML = '';
 };
