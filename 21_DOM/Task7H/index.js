78305877eadbe7b075628c752f88b063884a0baa
karrmao <karrmao@gmail.com>
2022-07-16 23:45:44 +0300

    c

--- 21_DOM/Task7H/index.js
+++ 21_DOM/Task7H/index.js
@@ -1,14 +1,4 @@
-// /*export*/ const setButton = buttonText => {
-//   const res = (document.querySelector('body').innerHTML = `<button>${buttonText}</button>`);
-//   console.dir(res);
-// };
-
-// setButton('Просто кнопка');
-
-//console.log('bb');
-
 export const createButton = buttonText => {
   document.createElement('button').body.append(`<button>${buttonText}</button>`);
 };
 /*export*/
-//document.creat('.categories').innerHTML = '';
