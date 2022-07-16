d59d2be1f8247622c68ceec3f477750cbd385cbd
karrmao <karrmao@gmail.com>
2022-07-17 01:08:57 +0300

    add

--- 21_DOM/Task7H/index.js
+++ 21_DOM/Task7H/index.js
@@ -1,11 +1,11 @@
-export const createButton = buttonText => {
+/*export*/ const createButton = buttonText => {
   //document.createElement('button');
   const button = document.createElement('button');
   console.log(button);
   button.textContent = buttonText;
   document.querySelector('body').append(button);
-  //return button;
+  return button;
 };
 // };
 /*export*/
-//console.log(createButton('Просто кнопка'));
+console.log(createButton('Просто кнопка'));
---
+++ 21_DOM/task8h/index.js
@@ -0,0 +1,21 @@
+export const finishList = () => {
+  const items = document.querySelector('.list');
+  const specialItem = items.querySelector('.special');
+
+  const firstItem = document.createElement('li');
+  firstItem.textContent = 1;
+  items.prepend(firstItem);
+
+  const thirdItem = document.createElement('li');
+  thirdItem.textContent = 4;
+  specialItem.before(thirdItem);
+
+  const sixthItem = document.createElement('li');
+  thirdItem.textContent = 6;
+  specialItem.after(thirdItem);
+
+  const lastItem = document.createElement('li');
+  lastItem.textContent = 8;
+  items.append(lastItem);
+};
+//finishList();/*export*/
