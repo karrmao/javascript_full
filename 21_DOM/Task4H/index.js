c0bbda05531fe88e8ba926bc606a87acd9f386e2
karrmao <karrmao@gmail.com>
2022-07-16 16:31:13 +0300

    c

--- 21_DOM/Task4H/index.js
+++ 21_DOM/Task4H/index.js
@@ -2,7 +2,7 @@ export const setTitle = text => {
   const addText = document.querySelector('.title');
   addText.textContent = text;
   //console.dir(addText);
-  return addText;
+  //return addText;
 };
 
 console.log(setTitle('lll'));
