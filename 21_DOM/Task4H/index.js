3bc991a2811a8b2fe837e5fb4a360ca7c240c1f4
karrmao <karrmao@gmail.com>
2022-07-16 15:41:15 +0300

    add

---
+++ 21_DOM/Task4H/index.js
@@ -0,0 +1,32 @@
+export const setTitle = text => {
+  const textContent = document.querySelector('.title').textContent;
+  //console.dir(textContent);
+  return textContent;
+};
+
+setTitle();
+
+// export const getDescription = () => {
+//   const innerText = document.querySelector('.about').innerText;
+//   //console.dir(innerText);
+//   return innerText;
+// };
+
+// getDescription();
+
+// export const getPlans = () => {
+//   const innerHTML = document.querySelector('.plans').innerHTML;
+//   //console.dir(innerHTML);
+//   return innerHTML;
+// };
+
+// getPlans();
+
+// export const getGoal = () => {
+//   const outerHTML = document.querySelector('.goal').outerHTML;
+//   //console.dir(outerHTML);
+//   return outerHTML;
+// };
+
+// getGoal();
+// // /*export*/
