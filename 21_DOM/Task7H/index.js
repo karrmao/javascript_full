418cf7c19b03f4ca02b8aea2d06d528b27fdffbc
karrmao <karrmao@gmail.com>
2022-07-16 23:54:31 +0300

    add

--- 21_DOM/Task7H/index.js
+++ 21_DOM/Task7H/index.js
@@ -1,5 +1,11 @@
 export const createButton = buttonText => {
-  document.createElement('button');
-  document.body.append(`<button>${buttonText}</button>`);
+  //document.createElement('button');
+  const button = document.createElement('button');
+  console.log(button);
+  button.textContent = buttonText;
+  //return button;
 };
+//   document.body.append(`<button>${buttonText}</button>`);
+// };
 /*export*/
+//console.log(createButton('Просто кнопка'));
