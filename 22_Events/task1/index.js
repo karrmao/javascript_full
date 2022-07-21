0e12feb91debabfd71a434b333da6f452c2a1937
karrmao <karrmao@gmail.com>
2022-07-21 21:07:32 +0300

    c

--- 22_Events/task1/index.js
+++ 22_Events/task1/index.js
@@ -3,10 +3,10 @@ const divElem = document.querySelector('.rect_div');
 const pElem = document.querySelector('.rect_p');
 const spanElem = document.querySelector('.rect_span');
 
-const clearBtn = document.querySelector('.clear-btn');
 const eventsList = document.querySelector('.events-list');
-const attachHandlersBtn = document.querySelector('.attach-handlers-btn');
+const clearBtn = document.querySelector('.clear-btn');
 const removeHandlersBtn = document.querySelector('.remove-handlers-btn');
+const attachHandlersBtn = document.querySelector('.attach-handlers-btn');
 
 const logTarget = (text, color) => {
   const eventListElem = document.querySelector('.events-list');
@@ -57,58 +57,3 @@ attachHandlersBtn.addEventListener('click', () => {
   pElem.addEventListener('click', logGreyP, true);
   spanElem.addEventListener('click', logGreySpan, true);
 });
-
-// const divElem = document.querySelector('.rect_div');
-// const pElem = document.querySelector('.rect_p');
-// const spanElem = document.querySelector('.rect_span');
-
-// const clearButton = document.querySelector('.clear-btn');
-// const eventList = document.querySelector('.events-list');
-// const attachHandless = document.querySelector('.attach-handlers-btn');
-// const removehHandless = document.querySelector('.remove-handlers-btn');
-
-// const logTarget = (text, color) => {
-//   const eventListElem = document.querySelector('.events-list');
-
-//   eventListElem.innerHTML += `<span style="color: ${color}; margin-left: 8px;">${text}</span>`;
-// };
-
-// const logGreenDiv = logTarget.bind(null, 'DIV', 'green');
-// const logGreenP = logTarget.bind(null, 'P', 'green');
-// const logGreenSpan = logTarget.bind(null, 'SPAN', 'green');
-
-// const logGreyDiv = logTarget.bind(null, 'DIV', 'grey');
-// const logGreyP = logTarget.bind(null, 'P', 'grey');
-// const logGreySpan = logTarget.bind(null, 'SPAN', 'grey');
-
-// divElem.addEventListener('click', logGreenDiv);
-// pElem.addEventListener('click', logGreenP);
-// spanElem.addEventListener('click', logGreenSpan);
-
-// divElem.addEventListener('click', logGreyDiv, true);
-// pElem.addEventListener('click', logGreyP, true);
-// spanElem.addEventListener('click', logGreySpan, true);
-
-// clearButton.addEventListener('click', () => {
-//   eventList.innerHTML = '';
-// });
-
-// removehHandless.addEventListener('click', () => {
-//   divElem.removeEventListener('click', logGreenDiv);
-//   pElem.removeEventListener('click', logGreenP);
-//   spanElem.removeEventListener('click', logGreenSpan);
-
-//   divElem.removeEventListener('click', logGreyDiv, true);
-//   pElem.removeEventListener('click', logGreyP, true);
-//   spanElem.removeEventListener('click', logGreySpan, true);
-// });
-
-// attachHandless.addEventListener('click', () => {
-//   divElem.addEventListener('click', logGreenDiv);
-//   pElem.addEventListener('click', logGreenP);
-//   spanElem.addEventListener('click', logGreenSpan);
-
-//   divElem.addEventListener('click', logGreyDiv, true);
-//   pElem.addEventListener('click', logGreyP, true);
-//   spanElem.addEventListener('click', logGreySpan, true);
-// });
