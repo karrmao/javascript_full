a3cffa30450b6424cb658ec118aa1dd1b765cf99
karrmao <karrmao@gmail.com>
2022-07-21 14:52:37 +0300

    c

--- 21_DOM/task12h/index.js
+++ 21_DOM/task12h/index.js
@@ -2,4 +2,4 @@ const getSection2 = num => {
   const findNumber = document.querySelector(`span[data-number="${num}"`);
   return findNumber.parentElement.dataset.section;
 };
-console.log(getSection2(5));
+//console.log(getSection2(5));
