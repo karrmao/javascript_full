f05370f2886bac2db96d43af9e11830ad83e2435
karrmao <karrmao@gmail.com>
2022-07-23 12:03:36 +0300

    add

---
+++ 23_events_2/task1/index.js
@@ -0,0 +1,54 @@
+/************/
+const emailInputElement = document.querySelector('#email');
+const passwordInputElement = document.querySelector('#password');
+
+const emailErrorElement = document.querySelector('.error-text_email');
+const passwordErrorElement = document.querySelector('.error-text_password');
+/************/ //Validators
+const isRequired = value => (value ? undefined : 'Required');
+const isEmail = value => (value.includes('@') ? undefined : 'Should be an email');
+/************/
+const validatorsByField = {
+  email: [isRequired, isEmail],
+  password: [isRequired],
+};
+
+const validate = (fieldName, value) => {
+  const validators = validatorsByField[fieldName];
+  return [isRequired, isEmail]
+    .map(validator => validator(value))
+    .filter(errorText => errorText)
+    .join(', ');
+};
+
+/************/
+const onEmailChange = event => {
+  const errorText = validate('email', event.target.value);
+  emailErrorElement.textContent = errorText;
+};
+
+emailInputElement.addEventListener('input', onEmailChange);
+/************/
+const onPasswordChange = event => {
+  const errorText = validate('password', event.target.value);
+  passwordErrorElement.textContent = errorText;
+};
+
+emailInputElement.addEventListener('input', onPasswordChange);
+/************/
+
+/************************************************************/
+const formElement = document.querySelector('.login-form');
+const onFormSubmit = event => {
+  event.preventDefault();
+  const formData = [...new FormData(formElement)].reduce(
+    (acc, [field, value]) => ({
+      ...acc,
+      [field]: value,
+    }),
+    {},
+  );
+
+  alert(JSON.stringify(formData));
+};
+formElement.addEventListener('submit', onFormSubmit);
